c1fe2af454ae8a686b70fc0156b59d50f7540e48
arthurmelton <amtitan@icloud.com>
2021-10-28 11:53:15 -0400

    works

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 echo "---cp tailwin.desktop /usr/share/xsessions---"
 sudo cp tailwin.desktop /usr/share/xsessions
-echo "---cp tialwin.png /usr/share/pixmaps---"
-sudo cp tialwin.png /usr/share/pixmaps
+echo "---cp tailwin.png /usr/share/pixmaps---"
+sudo cp tailwin.png /usr/share/pixmaps
 echo "---cargo build --release---"
 cargo build --release
-echo "---ln -f /target/release/tailwin /usr/bin/tailwin---"
-sudo ln -f /target/release/tailwin /usr/bin/tailwin
+echo "---ln -f ./target/release/tailwin /usr/bin/tailwin---"
+sudo ln -f ./target/release/Tailwin /usr/bin/tailwin
