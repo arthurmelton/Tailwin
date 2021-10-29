ddbdcb9ea086818be824242151616017bb5efdd9
arthurmelton <amtitan@icloud.com>
2021-10-28 20:23:24 -0400

    has my config

---
+++ example/tailwin.rs
@@ -0,0 +1,22 @@
+use std::process::Command;
+
+pub fn on_startup() {
+	Command::new("sh")
+		.args(&["-c", "/usr/bin/start-all.sh"])
+		.spawn()
+		.expect("failed to execute process");
+	Command::new("alacritty")
+		.spawn()
+		.expect("failed to execute process");
+}
+
+pub fn on_key(key:u32) -> String {
+	match key {
+		57 => {Command::new("firefox")
+		.spawn()
+		.expect("failed to execute process");},
+		53 => {return "destroy".to_string();},
+		_ => {Command::new("sh").args(&["-c", ["zenity --info --text='", key.to_string().as_str(), "'"].join("").as_str()]).spawn().expect("ERROR");},
+	}
+    return "null".to_string();
+}
